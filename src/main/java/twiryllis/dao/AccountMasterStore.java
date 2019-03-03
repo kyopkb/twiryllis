57b792a9bdb3ef4e964e31b219e94e2f17bae633
suga-kiyo <sign.pann@gmail.com>
2019-03-04 02:25:06 +0900

    [ADD] ユーザ情報を登録する処理を追加

--- src/main/java/twiryllis/dao/AccountMasterStore.java
+++ src/main/java/twiryllis/dao/AccountMasterStore.java
@@ -26,6 +26,51 @@ public class AccountMasterStore extends DataStoreBase {
 	}
 
 	/**
+	 * Register Twitter authentication information in the database
+	 * 
+	 * @param id  the user id of twitter account
+	 * @param accountName  the account name of twitter account
+	 * @param consumerKey  the Consumer Key of twitter api
+	 * @param consumerSecret  the Consumer Secret of twitter api
+	 * @param accessToken  the Access Token of twitter api
+	 * @param accessTokenSecret  the Access Token Secret of twitter api
+	 * @return true if registration is complete, false in case of failure
+	 * @since 0.1
+	 */
+	public boolean registerAccountConf(String id, String accountName, String consumerKey, String consumerSecret,
+			String accessToken, String accessTokenSecret) {
+
+		try {
+			StringBuilder sql = new StringBuilder();
+			sql.append(
+					"INSERT INTO account (id, accountName, consumerKey, consumerSecret, accessToken, accessTokenSecret) VALUES ");
+
+			String encryptConsumerKey = CipherManager.encrypt(consumerKey, Cipher.COMPOSITE_KEY, Cipher.ALGORITHM);
+			String encryptConsumerSecret = CipherManager.encrypt(consumerSecret, Cipher.COMPOSITE_KEY,
+					Cipher.ALGORITHM);
+			String encryptAccessToken = CipherManager.encrypt(accessToken, Cipher.COMPOSITE_KEY, Cipher.ALGORITHM);
+			String encryptAccessTokenSecret = CipherManager.encrypt(accessTokenSecret, Cipher.COMPOSITE_KEY,
+					Cipher.ALGORITHM);
+
+			sql.append("('" + id + "',");
+			sql.append("'" + accountName + "',");
+			sql.append("'" + encryptConsumerKey + "',");
+			sql.append("'" + encryptConsumerSecret + "',");
+			sql.append("'" + encryptAccessToken + "',");
+			sql.append("'" + encryptAccessTokenSecret + "');");
+
+			Statement stm = connection.createStatement();
+			stm.executeUpdate(sql.toString());
+            log.info("Insert complete: " + id);
+
+			return true;
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+			return false;
+		}
+	}
+
+	/**
 	 * Acquire authentication information to use Twitter API
 	 * 
 	 * @param id  the unique ID to identify the user
